9ebe6c23e6706345e69568b5eea6587e1f71977b
shivani <shivaniw69@gmail.com>
2025-02-04 00:13:34 +0530

    quicksort

--- DSA.cpp
+++ DSA.cpp
@@ -50,3 +50,55 @@ int main() {
      cout<<arr[i]<<"\t";
  }
 }
+// Online C++ compiler to run C++ program online
+////============================= QUIK SORT ALGORITHM========================================
+#include <iostream>
+using namespace std;
+int divide(int arr[],int low,int high){
+    int pv=arr[low];
+    int i=low+1;
+    int j=high;
+    do{
+        while(arr[i]<pv){
+            i++;
+        }
+        while(arr[j]>pv){
+            j--;
+        }
+        if(i<j){
+            int tmp; 
+            tmp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=tmp;
+        }
+    }
+    while(i<j);
+             int tmp; 
+            tmp=arr[low];
+            arr[low]=arr[j];
+            arr[j]=tmp;
+            return j;
+}
+void qksort(int arr[],int low,int high)
+{
+    int pv;
+    if(low<high){
+        pv=divide(arr,low,high);
+        qksort(arr,low,pv-1);
+        qksort(arr,pv+1,high);
+        
+    }
+}
+int main() {
+ int arr[]={9,2,1,50,6};
+ int size=sizeof(arr)/sizeof(arr[0]);
+ cout<<"\nBefore applying\n";
+ for(int i=0; i<size; i++){
+     cout<<arr[i]<<"\t";
+ }
+ qksort(arr,0,size-1);
+ cout<<"\nAfter applying quik sort\n";
+ for(int i=0; i<size; i++){
+     cout<<arr[i]<<"\t";
+ }
+}
