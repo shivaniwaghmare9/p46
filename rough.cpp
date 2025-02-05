99d57fc3aaad98765c5277c7db922ef3dc918dd2
shivani <shivaniw69@gmail.com>
2025-02-06 00:37:51 +0530

    new

---
+++ rough.cpp
@@ -0,0 +1,12 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+ 
+ int sum=1;
+ for (int i=1; i<=5;++i)
+ {
+     sum=sum*i;
+ }
+ cout<<sum;
+}
