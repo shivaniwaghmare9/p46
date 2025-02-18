09fd44654d36f2574da0e02b68345c5dc9931f19
shivani <shivaniw69@gmail.com>
2025-02-19 01:29:55 +0530

    sort

--- DSA.cpp
+++ DSA.cpp
@@ -332,3 +332,65 @@ int main() {
     }
    
 }  
+
+// Online C++ compiler to run C++ program online
+//==========================================merge sort================================
+#include <iostream>
+using namespace std;
+int divide(int arr[],int low,int high)
+{
+    int pv=arr[low];
+    int i=low+1;
+    int j=high;
+do{
+    while(arr[i]<pv){
+        i++;
+    }
+    while(arr[j]>pv){
+        j--;
+    }
+    if(i<j)
+    {
+        int tmp;
+        tmp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=tmp;
+    }
+    
+}
+while(i<j);
+int tmp;
+tmp=arr[low];
+arr[low]=arr[j];
+arr[j]=tmp;
+return j;
+
+}
+void quick(int arr[],int low,int high)
+{
+    int pv;
+    if(low<high)
+    {
+        pv=divide(arr,low,high);
+        quick(arr,low,pv-1);
+        quick(arr,pv+1,high);
+    }
+}
+
+int main() {
+    
+    int arr[]={5,4,3,2,1};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    cout<<"Before applying\n";
+    for(int i=0; i<size; i++)
+    {
+        cout<<arr[i]<<"\t";
+    }
+    quick(arr,0,size);
+    cout<<"\nAfter applying\n";
+    for(int i=0; i<size; i++)
+    {
+        cout<<arr[i]<<"\t";
+    }
+ 
+}
