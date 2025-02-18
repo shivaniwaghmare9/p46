529d86f7dbbf982fc7152daff924a3854b77104c
shivani <shivaniw69@gmail.com>
2025-02-18 12:10:09 +0530

    sort

--- rough.cpp
+++ rough.cpp
@@ -57,3 +57,51 @@ int main() {
     
    
 }
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+class top{
+    int c=0;
+    bool f=true;
+    public:
+    void bubble(int arr[],int s)
+    {
+        for(int i=0; i<s; i++)
+        {
+            for(int j=0; j<s-i-1; j++)
+            {
+                c++;
+                if(arr[j]>arr[j+1]){
+                    int tmp;
+                    tmp=arr[j+1];
+                    arr[j+1]=arr[j];
+                    arr[j]=tmp;
+                    f=false;
+                    
+                }
+            }
+            if(f==true){
+                break;
+            }
+        }
+        cout<<"\ncounter hit="<<c;
+    }
+    
+};
+int main() {
+    top p;
+    int arr[]={1,2,3,4,5};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    cout<<"Before apllying\n";
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<"\t";
+    }
+    p.bubble(arr,size);
+    cout<<"\nAfter applying\n";
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<"\t";
+    }
+    
+    
+}
