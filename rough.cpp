a228ba0d7c81ab574ab2c98de81bd8698b9d9f1a
shivani <shivaniw69@gmail.com>
2025-02-19 11:34:15 +0530

    new

--- rough.cpp
+++ rough.cpp
@@ -617,3 +617,57 @@ int main() {
     }
  
 }
+// Online C++ compiler to run C++ program online
+////=======================================quick sort===============================
+#include <iostream>
+using namespace std;
+int divide(int arr[],int low,int high){
+    int pv=arr[low];
+    int i=low+1;
+    int j=high;
+    do{
+        while(arr[i]<pv){
+            i++;
+        }
+        while(arr[j]>pv){
+            j--;
+        }
+        if(i<j){
+            int tmp;
+            tmp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=tmp;
+            
+        }
+    }
+    while(i<j);
+    int tmp;
+    tmp=arr[low];
+    arr[low]=arr[j];
+    arr[j]=tmp;
+    return j;
+}
+void quick(int arr[],int low,int high){
+    int pv;
+    if(low<high){
+        pv=divide(arr,low,high);
+        quick(arr,low,pv-1);
+        quick(arr,pv+1,high);
+    }
+}
+
+int main() {
+   
+    int arr[]={6,3,7,4,5,2,1};
+    int size=sizeof(arr)/sizeof(arr[0]);
+    cout<<"Before Applying\n";
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<"\t";
+    }
+    quick(arr,0,size);
+    cout<<"\nAfter Applying\n";
+    for(int i=0; i<size; i++){
+        cout<<arr[i]<<"\t";
+    }
+   
+}
