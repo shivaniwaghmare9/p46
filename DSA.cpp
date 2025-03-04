0449d27ad0e85b250b030bfd18a2ee9736f10f29
shivani <shivaniw69@gmail.com>
2025-03-05 01:24:20 +0530

    2dvector

--- DSA.cpp
+++ DSA.cpp
@@ -1044,3 +1044,37 @@ int main() {
         cout<<get<0>(v[i])<<":"<<get<1>(v[i])<<":"<<get<2>(v[i])<<"\n";
     }
 }
+// Online C++ compiler to run C++ program online
+//===================================2D VECTOR================================================================
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    vector<vector<int>>v1;
+    int row,col,v;
+    cout<<"Enter no of row\n";
+    cin>>row;
+    cout<<"Enter no of coulmns\n";
+    cin>>col;
+    for(int i=1; i<=row; i++){
+        vector<int>v2;
+        for(int j=1; j<=col; j++){
+            cout<<"enter values\n";
+            cin>>v;
+            v2.push_back(v);
+        }
+        v1.push_back(v2);
+    }
+    cout<<"\n stored value are\n";
+    for(int i=0; i<v1.size();i++){
+        for(int j=0; j<v1[i].size();j++){
+            cout<<v1[i][j]<<"\t";
+        }
+         cout<<"\n";
+    }
+   
+    
+    
+    
+}
