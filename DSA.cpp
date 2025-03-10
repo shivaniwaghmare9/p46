2975462f68a7e701fabf726a1530aff321ab33fc
shivani <shivaniw69@gmail.com>
2025-03-10 15:01:21 +0530

    new

--- DSA.cpp
+++ DSA.cpp
@@ -1515,7 +1515,151 @@ for(int i=0; i<sum.size();i++){
 }
 
 }
+// Online C++ compiler to run C++ program online
+//===================================paranthesess====================================================================================================
+#include <iostream>
+using namespace std;
+#include<stack>
+bool pvalid(string s)
+{
+    stack<char>stk;
+    bool r=true;
+    for(int i=0; i<s.size();i++)
+    {
+        if(s[i]=='{' || s[i]=='(' || s[i]=='[')
+        {
+            stk.push(s[i]);
+        }
+        else if(s[i]=='}')
+        {
+            if(!stk.empty() && stk.top()=='{')
+            {
+                stk.pop();
+            }
+            else
+            {
+                r=false;
+                break;
+            }
+            
+        }
+        else if(s[i]==')')
+        {
+            if(!stk.empty() && stk.top()=='(')
+            {
+                stk.pop();
+            }
+            else
+            {
+                r=false;
+                break;
+            }
+            
+        }
+        else if(s[i]==']')
+        {
+            if(!stk.empty() && stk.top()=='[')
+            {
+                stk.pop();
+            }
+            else
+            {
+                r=false;
+                break;
+            }
+            
+        }
+    }
+    if(!stk.empty())
+    {
+        return false;
+    }
+    else{
+        return r;
+    }
+}
 
+int main() {
+    string s="{[()]}";
+    if(pvalid(s))
+    {
+        cout<<"valid";
+    }
+    else{
+        cout<<"invalid";
+    }
+}
+/******************************************************************************
+
+
+*******************************************************************************/
+#include <iostream>
+#include<stack>
+using namespace std;
+bool pvalid(string s){
+    stack<char>stk;
+    bool r=true;
+    for(int i=0; i<s.size();i++){
+        if(s[i]=='{' ||s[i]=='(' ||s[i]=='['){
+            stk.push(s[i]);
+        }
+        else if(s[i]=='}')
+        {
+            if(!stk.empty() && stk.top()=='{')
+            {
+                stk.pop();
+            }
+            else
+            {
+                r=false;
+                break;
+            }
+        }
+        else if(s[i]==')')
+        {
+            if(!stk.empty() && stk.top()=='(')
+            {
+                stk.pop();
+            }
+            else
+            {
+                r=false;
+                break;
+            }
+        }
+        else if(s[i]==']')
+        {
+            if(!stk.empty() && stk.top()=='[')
+            {
+                stk.pop();
+            }
+            else
+            {
+                r=false;
+                break;
+            }
+        }
+    }
+    if(!stk.empty())
+    {
+        return false;
+    }
+    else
+    {
+        return r;
+    }
+}
+
+int main()
+{
+    string s="{[()]}";
+    if(pvalid(s)){
+        cout<<"valid";
+    }
+    else{
+        cout<<"invalid";
+    }
+}
 
 
 
