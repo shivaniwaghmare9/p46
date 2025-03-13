b45d5c6c35033a37f27027ce37bc9b0a720b2cdc
shivani <shivaniw69@gmail.com>
2025-03-14 00:11:14 +0530

    new

--- rough.cpp
+++ rough.cpp
@@ -924,6 +924,26 @@ cout<<"enter the number";
 cin>>num;
 (num>0)?cout<<"positive":(num<0)?cout<<"negative":cout<<"zero";
 }
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+int main() {
+  int sn=6;
+  int In=7;
+  int lcm;
+  for(int i=In;;i=i+In)
+  {
+      if(i%sn==0 && i%In==0)
+      {
+          lcm=i;
+          break;
+      }
+  }
+  cout<<lcm;
+}
+     
+     
+
 
 
 
