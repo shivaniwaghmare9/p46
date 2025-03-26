cffdbf4a0c16682e504f86f23e3ce0d8cd9d1428
shivani <shivaniw69@gmail.com>
2025-03-27 01:19:36 +0530

    work

--- DSA.cpp
+++ DSA.cpp
@@ -2370,7 +2370,40 @@ int main(){
     
 }
       
-            
+  // Online C++ compiler to run C++ program online
+//===========================template========================================
+#include <iostream>
+using namespace std;
+
+template <typename t1>
+t1 sum(t1 a,t1 b)
+{
+    return a+b;
+}
+int main()
+{
+    cout<<sum(7,2);
+}
+// Online C++ compiler to run C++ program online
+//===========================template(sum&multi)========================================
+#include <iostream>
+using namespace std;
+
+template <typename t1>
+t1 sum(t1 a,t1 b)
+{
+    return a+b;
+}
+template <typename t1,typename t2>
+int multi(t1 x1,t2 x2)
+{
+    return x1*x2;
+}
+int main()
+{
+    cout<<sum(7,2)<<"\n";
+    cout<<multi(7,2);
+}          
       
        
 
