3d059e53269dc70097b815628eddbf406f8fc031
shivani <shivaniw69@gmail.com>
2025-04-14 11:45:07 +0530

    dsa

--- workdsa.cpp
+++ workdsa.cpp
@@ -32,3 +32,76 @@ int main()
 //     cout<<sum(4,5)<<"\n";
 //     cout<<multi(7,8)<<"\n";
 // }
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+template<typename t1>
+class cybrom{
+    public:t1 show(t1 a)
+    {
+        return a++;
+    }
+};
+template<typename t1,typename t2>
+class bhopal
+{
+    t1 x1;
+    t2 x2;
+    public:bhopal(t1 a,t2 b)
+    {
+        x1=a;
+        x2=b;
+    }
+    void show()
+    {
+        cout<<"x1="<<x1<<"\n";
+         cout<<"x2="<<x2<<"\n";
+    }
+};
+int main()
+{
+    cybrom<char>obj;
+    cout<<obj.show('a')<<"\n";
+    bhopal<int,int>bp(30,20);
+    bp.show();
+}
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+namespace cybrom
+{
+    void student()
+    {
+        cout<<"cybrom student\n";
+    }
+    namespace bhopal
+    {
+        void student()
+        {
+            cout<<"bhopal student\n";
+        }
+    }
+    void show()
+    {
+        cout<<"first program\n";
+    }
+    class iostreamnew
+    {
+    public:void show()
+    {
+        cout<<"\nclass example\n";
+    }
+    }
+    coutnew;
+}
+using namespace cybrom;
+int main()
+{
+    student();
+     bhopal::student();
+     show();
+     iostreamnew obj;
+     obj.show();
+     coutnew.show();
+}
